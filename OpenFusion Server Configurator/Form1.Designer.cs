f88753d92002f2e440fa055d4ce6d32a039cdc0a
Onii-chan <failer007@live.dk>
2020-10-31 23:47:12 +0100

    Disabled login server ip textbox + added icon
    Disabled the login server ip textbox until it's needed. Additionally fixed tab indexes.

--- OpenFusion Server Configurator/Form1.Designer.cs
+++ OpenFusion Server Configurator/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.loginServerGroupBox = new System.Windows.Forms.GroupBox();
             this.lsIpAddressTxtBox = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
@@ -75,11 +76,12 @@
             // 
             // lsIpAddressTxtBox
             // 
+            this.lsIpAddressTxtBox.Enabled = false;
             this.lsIpAddressTxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lsIpAddressTxtBox.Location = new System.Drawing.Point(6, 34);
             this.lsIpAddressTxtBox.Name = "lsIpAddressTxtBox";
             this.lsIpAddressTxtBox.Size = new System.Drawing.Size(188, 20);
-            this.lsIpAddressTxtBox.TabIndex = 3;
+            this.lsIpAddressTxtBox.TabIndex = 2;
             this.lsIpAddressTxtBox.Text = "127.0.0.1";
             // 
             // label2
@@ -89,7 +91,7 @@
             this.label2.Location = new System.Drawing.Point(6, 18);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(57, 13);
-            this.label2.TabIndex = 2;
+            this.label2.TabIndex = 1;
             this.label2.Text = "Ip Address";
             // 
             // lsPortTxtBox
@@ -98,7 +100,7 @@
             this.lsPortTxtBox.Location = new System.Drawing.Point(6, 75);
             this.lsPortTxtBox.Name = "lsPortTxtBox";
             this.lsPortTxtBox.Size = new System.Drawing.Size(188, 20);
-            this.lsPortTxtBox.TabIndex = 1;
+            this.lsPortTxtBox.TabIndex = 4;
             this.lsPortTxtBox.Text = "8001";
             // 
             // label1
@@ -108,7 +110,7 @@
             this.label1.Location = new System.Drawing.Point(6, 59);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(26, 13);
-            this.label1.TabIndex = 0;
+            this.label1.TabIndex = 3;
             this.label1.Text = "Port";
             // 
             // groupBox1
@@ -121,7 +123,7 @@
             this.groupBox1.Location = new System.Drawing.Point(218, 12);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(200, 110);
-            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Game Server";
             // 
@@ -131,7 +133,7 @@
             this.gsIpAddressTxtBox.Location = new System.Drawing.Point(6, 34);
             this.gsIpAddressTxtBox.Name = "gsIpAddressTxtBox";
             this.gsIpAddressTxtBox.Size = new System.Drawing.Size(188, 20);
-            this.gsIpAddressTxtBox.TabIndex = 3;
+            this.gsIpAddressTxtBox.TabIndex = 7;
             this.gsIpAddressTxtBox.Text = "127.0.0.1";
             // 
             // label3
@@ -141,7 +143,7 @@
             this.label3.Location = new System.Drawing.Point(6, 18);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(57, 13);
-            this.label3.TabIndex = 2;
+            this.label3.TabIndex = 6;
             this.label3.Text = "Ip Address";
             // 
             // gsPortTxtBox
@@ -150,7 +152,7 @@
             this.gsPortTxtBox.Location = new System.Drawing.Point(6, 75);
             this.gsPortTxtBox.Name = "gsPortTxtBox";
             this.gsPortTxtBox.Size = new System.Drawing.Size(188, 20);
-            this.gsPortTxtBox.TabIndex = 1;
+            this.gsPortTxtBox.TabIndex = 9;
             this.gsPortTxtBox.Text = "8002";
             // 
             // label4
@@ -160,7 +162,7 @@
             this.label4.Location = new System.Drawing.Point(6, 59);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(26, 13);
-            this.label4.TabIndex = 0;
+            this.label4.TabIndex = 8;
             this.label4.Text = "Port";
             // 
             // groupBox2
@@ -170,7 +172,7 @@
             this.groupBox2.Location = new System.Drawing.Point(12, 128);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(406, 255);
-            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabIndex = 10;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Game Features";
             // 
@@ -191,7 +193,7 @@
             this.panel1.Location = new System.Drawing.Point(3, 18);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(400, 234);
-            this.panel1.TabIndex = 9;
+            this.panel1.TabIndex = 11;
             // 
             // mobsEnabledCheckBox
             // 
@@ -199,9 +201,9 @@
             this.mobsEnabledCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.mobsEnabledCheckBox.Location = new System.Drawing.Point(260, 52);
             this.mobsEnabledCheckBox.Name = "mobsEnabledCheckBox";
-            this.mobsEnabledCheckBox.Size = new System.Drawing.Size(94, 17);
-            this.mobsEnabledCheckBox.TabIndex = 11;
-            this.mobsEnabledCheckBox.Text = "Mobs Enabled";
+            this.mobsEnabledCheckBox.Size = new System.Drawing.Size(114, 17);
+            this.mobsEnabledCheckBox.TabIndex = 15;
+            this.mobsEnabledCheckBox.Text = "Allow Mob Combat";
             this.mobsEnabledCheckBox.UseVisualStyleBackColor = true;
             // 
             // motdTxtBox
@@ -210,7 +212,7 @@
             this.motdTxtBox.Location = new System.Drawing.Point(3, 76);
             this.motdTxtBox.Name = "motdTxtBox";
             this.motdTxtBox.Size = new System.Drawing.Size(394, 106);
-            this.motdTxtBox.TabIndex = 10;
+            this.motdTxtBox.TabIndex = 17;
             this.motdTxtBox.Text = "Welcome to OpenFusion!";
             // 
             // label8
@@ -220,7 +222,7 @@
             this.label8.Location = new System.Drawing.Point(3, 60);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(39, 13);
-            this.label8.TabIndex = 9;
+            this.label8.TabIndex = 16;
             this.label8.Text = "MOTD";
             // 
             // label6
@@ -230,7 +232,7 @@
             this.label6.Location = new System.Drawing.Point(3, 11);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(76, 13);
-            this.label6.TabIndex = 0;
+            this.label6.TabIndex = 12;
             this.label6.Text = "Account Level";
             // 
             // eventCrateDropChanceTxtBox
@@ -239,7 +241,7 @@
             this.eventCrateDropChanceTxtBox.Location = new System.Drawing.Point(203, 202);
             this.eventCrateDropChanceTxtBox.Name = "eventCrateDropChanceTxtBox";
             this.eventCrateDropChanceTxtBox.Size = new System.Drawing.Size(194, 20);
-            this.eventCrateDropChanceTxtBox.TabIndex = 8;
+            this.eventCrateDropChanceTxtBox.TabIndex = 21;
             this.eventCrateDropChanceTxtBox.Text = "10";
             // 
             // customNameCheckBox
@@ -249,7 +251,7 @@
             this.customNameCheckBox.Location = new System.Drawing.Point(260, 29);
             this.customNameCheckBox.Name = "customNameCheckBox";
             this.customNameCheckBox.Size = new System.Drawing.Size(120, 17);
-            this.customNameCheckBox.TabIndex = 3;
+            this.customNameCheckBox.TabIndex = 14;
             this.customNameCheckBox.Text = "Allow Custom Name";
             this.customNameCheckBox.UseVisualStyleBackColor = true;
             // 
@@ -260,7 +262,7 @@
             this.label7.Location = new System.Drawing.Point(206, 185);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(129, 13);
-            this.label7.TabIndex = 7;
+            this.label7.TabIndex = 20;
             this.label7.Text = "Event Crate Drop Chance";
             // 
             // accountLevelComboBox
@@ -270,16 +272,17 @@
             this.accountLevelComboBox.Location = new System.Drawing.Point(3, 27);
             this.accountLevelComboBox.Name = "accountLevelComboBox";
             this.accountLevelComboBox.Size = new System.Drawing.Size(242, 21);
-            this.accountLevelComboBox.TabIndex = 4;
+            this.accountLevelComboBox.TabIndex = 13;
             // 
             // eventModeComboBox
             // 
             this.eventModeComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.eventModeComboBox.FormattingEnabled = true;
+            this.eventModeComboBox.ItemHeight = 13;
             this.eventModeComboBox.Location = new System.Drawing.Point(3, 201);
             this.eventModeComboBox.Name = "eventModeComboBox";
             this.eventModeComboBox.Size = new System.Drawing.Size(188, 21);
-            this.eventModeComboBox.TabIndex = 6;
+            this.eventModeComboBox.TabIndex = 19;
             // 
             // label5
             // 
@@ -288,7 +291,7 @@
             this.label5.Location = new System.Drawing.Point(3, 185);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(65, 13);
-            this.label5.TabIndex = 5;
+            this.label5.TabIndex = 18;
             this.label5.Text = "Event Mode";
             // 
             // loadBtn
@@ -296,7 +299,7 @@
             this.loadBtn.Location = new System.Drawing.Point(262, 389);
             this.loadBtn.Name = "loadBtn";
             this.loadBtn.Size = new System.Drawing.Size(75, 23);
-            this.loadBtn.TabIndex = 6;
+            this.loadBtn.TabIndex = 23;
             this.loadBtn.Text = "Load";
             this.loadBtn.UseVisualStyleBackColor = true;
             this.loadBtn.Click += new System.EventHandler(this.loadBtn_Click);
@@ -306,7 +309,7 @@
             this.saveBtn.Location = new System.Drawing.Point(343, 389);
             this.saveBtn.Name = "saveBtn";
             this.saveBtn.Size = new System.Drawing.Size(75, 23);
-            this.saveBtn.TabIndex = 7;
+            this.saveBtn.TabIndex = 24;
             this.saveBtn.Text = "Save";
             this.saveBtn.UseVisualStyleBackColor = true;
             this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
@@ -317,7 +320,7 @@
             this.linkLabel1.Location = new System.Drawing.Point(12, 401);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(79, 13);
-            this.linkLabel1.TabIndex = 8;
+            this.linkLabel1.TabIndex = 22;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "View on Github";
             this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
@@ -334,6 +337,7 @@
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.loginServerGroupBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
